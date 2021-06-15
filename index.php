ed106a9bd98bd9955117ceda80e304b4ac4d4a58
Shreya Kulkarni <shreya11031@gmail.com>
2021-06-15 14:59:52 +0530

    updated index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-//if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!= true)
-//{
-  //  header("Location: login.php "); 
-//}
-?>
+// if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!= true)
+// {
 
+//    header("Location: login.php "); 
+// }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -48,31 +48,26 @@ session_start();
               <li><a href="#benefits">Benefits</a></li>
               <li class="drop-down"><a href="#">Student</a>
                 <ul>
+                  <li><a href="signin1.php">Sign in</a></li>
                   <li><a href="#jobs">Find Jobs</a></li>
-                  <li><a href="#">Testimonial</a></li>
                 </ul>
               </li>
               <li class="drop-down"><a href="#">Recruiter</a>
                 <ul>
+                <li><a href="teachersignin.php">Sign in</a></li>
                   <li><a href="#jobs">Post Jobs</a></li>
-                  <li><a href="#">Testimonial</a></li>
                 </ul>
               </li>
               <li><a href="#contact">Contact Us</a></li>
-
-
             </ul>
           </nav><!-- .nav-menu -->
-          <a href="logout.php" class="get-started-btn scrollto">Log out</a>
+          <a href="login.php" class="get-started-btn scrollto">Log in</a>
+          <a href="admin/newadminlogin.php" class="get-started-btn scrollto">Admin Log in</a>
+          <!--<a href="signin1.php" class="get-started-btn scrollto">Sign in</a>-->
         </div>
       </div>
     </div>
   </header><!-- End Header -->
-    <?php
-        $username = $_GET['username'];
-        $_SESSION['username'] = $username;
-        setcookie('username',$username,time() + (86400 * 30) ,"/");
-    ?>
   <!-- ======= home Section ======= -->
   <section id="home" class="d-flex flex-column justify-content-center">
     <div class="container">
@@ -80,7 +75,8 @@ session_start();
         <div class="col-xl-8">
           <h1>Find your Dream Job</h1>
           <h2>Your internship/job solution in your own campus!</h2>
-          <h2>Welcome back <?php echo $_SESSION['username']?>!</h2>
+          <h2>Sign in to get a student/recruiter account!</h2>
+
         </div>
       </div>
     </div>
@@ -94,28 +90,25 @@ session_start();
 
         <div class="section-title">
           <h2>About Us</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>We help you find perfect internship in your own campus!!</p>
         </div>
 
         <div class="row content">
           <div class="col-lg-6">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+              Our Portal creates an opportunity for both internship seekers and startups or professors to embrace an easy employement process. 
             </p>
             <ul>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
-              <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-              <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+              <li><i class="ri-check-double-line"></i> You can register for free.</li>
+              <li><i class="ri-check-double-line"></i> Time saving and help streamline the right candidate/internship.</li>
+              <li><i class="ri-check-double-line"></i>Get to know all in-house internship opportunities at one place.</li>
             </ul>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0">
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a href="#" class="btn-learn-more">Learn More</a>
+            <h4>
+            So do not hesitate to explore your career opportunities with our job portal and give your career the elevation that you have always been waiting for.
+            </h4>
+           
           </div>
         </div>
 
@@ -127,56 +120,64 @@ session_start();
       <div class="container">
 
         <div class="section-title">
-          <h2>Jobs</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <h2>Internships</h2>
+          <p>IN-HOUSE INTERNSHIPS(WITHIN THE CAMPUS)</p>
         </div>
 
         <div class="row">
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href="">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <h4><a href="https://kjsce.somaiya.edu/en/internship-cell" target = "_blank">Internships offered by faculties(Inter/Intra department)</a></h4>
+              
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-file"></i></div>
-              <h4><a href="">Sed ut perspiciatis</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+              <h4><a href="https://www.bloomboxkjsce.com/" target = "_blank">Bloombox : E-Cell KJSCE</a></h4>
+              
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-tachometer"></i></div>
-              <h4><a href="">Magni Dolores</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+              <h4><a href="https://kjsce.somaiya.edu/en/internship-cell" target = "_blank">Software Development Cell</a></h4>
+     
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-world"></i></div>
-              <h4><a href="">Nemo Enim</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+              <h4><a href="https://kjsce.somaiya.edu/en/internship-cell" target = "_blank">Mega Projects</a></h4>
+              <p>They Include Orion, Onyx , Robocon etc.</p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-slideshow"></i></div>
-              <h4><a href="">Dele cardo</a></h4>
-              <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+              <h4><a href="http://www.riidl.org/" target = "_blank">Campus Companies: StartUps started by the Students</a></h4>
+              <p></p>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-slideshow"></i></div>
+              <h4><a href="https://www.facebook.com/kjsce.Parvaah/" target = "_blank">Parvaha:Social Initiative of KJSCE</a></h4>
+              <p></p>
             </div>
           </div>
 
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
             <div class="icon-box">
               <div class="icon"><i class="bx bx-arch"></i></div>
-              <h4><a href="">Divera don</a></h4>
-              <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+              <h4><a href="">Library / Department / Institute / Campus administration</a></h4>
+          
             </div>
           </div>
 
@@ -217,63 +218,7 @@ session_start();
     </section><!-- End Features Section -->
 
     <!-- ======= Clients Section ======= -->
-    <section id="clients" class="clients">
-      <div class="container">
-
-        <div class="row no-gutters clients-wrap clearfix wow fadeInUp">
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-1.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-2.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-3.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-4.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-5.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-6.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-7.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-          <div class="col-lg-3 col-md-4 col-xs-6">
-            <div class="client-logo">
-              <img src="assets/img/clients/client-8.png" class="img-fluid" alt="">
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Clients Section -->
+    
     <!-- ======= Contact Section ======= -->
     <section id="contact" class="contact">
       <div class="container">
@@ -310,8 +255,8 @@ session_start();
           </div>
 
           <div class="col-lg-8 mt-5 mt-lg-0">
-
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <h4 class="sent-notification"></h4>
+            <form action="contact.php" method="post" role="form" id="myForm" class="php-email-form">
               <div class="form-row">
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
@@ -327,7 +272,7 @@ session_start();
                 <div class="validate"></div>
               </div>
               <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <textarea class="form-control" id="message" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Write your Message"></textarea>
                 <div class="validate"></div>
               </div>
               <div class="mb-3">
@@ -335,7 +280,7 @@ session_start();
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
+              <div class="text-center"><button type="submit" onClick="sendEmail()" name="submit">Send Message</button></div>
             </form>
 
           </div>
@@ -359,7 +304,7 @@ session_start();
       </div>
       <div class="copyright">
         Helping you find your dream job opportunities...
-        &copy; Copyright <strong><span>Inter123</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Intern123</span></strong>. All Rights Reserved
       </div>
     </div>
   </footer><!-- End Footer -->
@@ -371,7 +316,7 @@ session_start();
   <script src="assets/vendor/jquery/jquery.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <!--<script src="assets/vendor/php-email-form/validate.js"></script>-->
   <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
   <script src="assets/vendor/counterup/counterup.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
@@ -381,6 +326,44 @@ session_start();
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   
+   <script type="text/javascript">
+     function sendEmail(){
+       var name = $("#name");
+       var email = $("#email");
+       var subject = $("#subject");
+       var message = $("#message");
+
+       if(isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(subject) && isNotEmpty(message)){
+         $.ajax({
+           url: 'contact.php',
+           method: 'POST',
+           dataType : 'json',
+           data : {
+             name: name.val(),
+             email: email.val(),
+             subject: subject.val(),
+             message: message.val()
+           }, success : function(response){
+                 $('#myForm')[0].reset();
+                 $('.sent-notification').text("Message Sent Successfully");
+           }
+         });
+       }
+     }
+
+     funtion isNotEmpty(caller){
+       if(caller.val()== ""){
+         caller.css('border', '1px solid red');
+         return false;
+       }else{
+         caller.css('border' , '');
+         return true;
+       }
+     }
+   
+   </script>
 </body>
 
 </html>
