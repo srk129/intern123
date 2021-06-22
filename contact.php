7ecdca6a769c34596c5413d697dffc6e6bf4f488
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2021-06-22 16:24:11 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit'])){
   $email = $_POST['email'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
-  $from = "vaibhavikundle20@gmail.com";
+  $from = "shopex247@gmail.com";
   $headers = "From: $from";
 
   require_once "PHPMailer/PHPMailer.php";
@@ -19,14 +19,14 @@ try{
   $mail->isSMTP();
   $mail->Host = "smtp.gmail.com";
   $mail->SMTPAuth = true;
-  $mail->Username = "vaibhavikundle20@gmail.com";
-  $mail->Password = "Exosuho2205";
+  $mail->Username = "shopex247@gmail.com";
+  $mail->Password = "localshope";
   $mail->Port = 465;
   $mail->SMTPSecure = "ssl";
 
   $mail->isHTML(true);
   $mail->setFrom($email,"Intern123");
-  $mail->addAddress("vaibhavikundle20@gmail.com");
+  $mail->addAddress("shopex247@gmail.com");
   $mail->Subject = ("$subject");
   $mail->Body = ("Enquiry:$message,Name of sender:$name,Email address of:$email");
   $mail->send();
