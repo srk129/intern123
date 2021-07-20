792c9f313485a317f600adfa9a64d64f3b62a5a1
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2021-07-20 21:17:21 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,6 +1,6 @@
 <?php
 // connect to the database
-$conn = mysqli_connect'remotemysql.com', 'UfLOaliJqO', '4PlQ2COs67', "UfLOaliJqO");
+$conn = mysqli_connect('remotemysql.com', 'UfLOaliJqO', '4PlQ2COs67', "UfLOaliJqO");
 // check connection
 if(!$conn){
 echo 'Connection error: '. mysqli_connect_error();
